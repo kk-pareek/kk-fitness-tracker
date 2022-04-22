15ccc95d0c04ce88a8dd7cc059fd3b9e2a30c36a
Krishan Kumar Pareek <k.k.pareek18@gmail.com>
2022-04-23 00:31:12 +0530

    Add signup feature

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { DateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  maxDate = new Date();
+
+  signUpForm = this.theFormBuilder.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+    dateOfBirth: ['', [Validators.required]]
+  });
+
+  constructor(
+    private theFormBuilder: FormBuilder,
+    private theDateAdapter: DateAdapter<Date>) {
+      this.theDateAdapter.setLocale('en-GB');
+  }
 
   ngOnInit(): void {
+    this.maxDate = new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+  }
+
+  onSignUp() {
+    console.log(this.signUpForm.value);
+  }
+
+  get name() {
+    return this.signUpForm.get('name');
+  }
+
+  get email() {
+    return this.signUpForm.get('email');
+  }
+
+  get password() {
+    return this.signUpForm.get('password');
+  }
+
+  get dateOfBirth() {
+    return this.signUpForm.get('dateOfBirth');
   }
 
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,33 @@
 import { NgModule } from '@angular/core';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule}  from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
-  imports: [ MatSliderModule ],
-  exports: [ MatSliderModule ]
+  imports: [ 
+    MatSliderModule, 
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule
+  ],
+  exports: [
+    MatSliderModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule
+  ]
 })
 export class MaterialModule { }

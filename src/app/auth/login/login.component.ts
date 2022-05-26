808dc59d6350930ad083d0daaa7e21177b89cd2d
Krishan Kumar Pareek <k.k.pareek18@gmail.com>
2022-05-26 12:13:29 +0530

    Add login feature

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { DateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  maxDate = new Date();
 
-  ngOnInit(): void {
+  loginForm = this.theFormBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+  });
+
+  constructor(
+    private theFormBuilder: FormBuilder,
+    private theDateAdapter: DateAdapter<Date>) {
+      this.theDateAdapter.setLocale('en-GB');
   }
 
+  ngOnInit(): void {
+    this.maxDate = new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+  }
+
+  onLogin() {
+    console.log(this.loginForm.value);
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
 }

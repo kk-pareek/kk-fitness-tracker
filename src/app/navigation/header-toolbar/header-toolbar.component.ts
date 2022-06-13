23cf8378eb106f4895015a2bbe37ab016d78d244
Krishan Kumar Pareek <k.k.pareek18@gmail.com>
2022-06-13 14:27:54 +0530

    Use rxjs to manage isAuth() state

--- src/app/navigation/header-toolbar/header-toolbar.component.ts
+++ src/app/navigation/header-toolbar/header-toolbar.component.ts
@@ -1,21 +1,36 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthServiceService } from 'src/app/auth/auth-service.service';
 
 @Component({
   selector: 'app-header-toolbar',
   templateUrl: './header-toolbar.component.html',
   styleUrls: ['./header-toolbar.component.css']
 })
-export class HeaderToolbarComponent implements OnInit {
+export class HeaderToolbarComponent implements OnInit, OnDestroy {
 
   @Output() toggleSideNav = new EventEmitter;
+  isAuth = false;
+  authSubscription!: Subscription;
 
-  constructor() { }
+  constructor(private theAuthService: AuthServiceService) {}
 
   ngOnInit(): void {
+    this.authSubscription = this.theAuthService.authStatus.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    })
   }
 
   onToggleSideNav() {
     this.toggleSideNav.emit();
+  }
+
+  onLogout() {
+    this.theAuthService.logout();
+  }
+
+  ngOnDestroy() {
+    this.authSubscription.unsubscribe();
   }
 
 }

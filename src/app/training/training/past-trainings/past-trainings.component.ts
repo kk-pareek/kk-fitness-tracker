4b1478912633111310736d7bcc3f28410ccd3cc1
Krishan Kumar Pareek <k.k.pareek18@gmail.com>
2022-06-15 11:23:45 +0530

    Update mat-table, add s.no. column

--- src/app/training/training/past-trainings/past-trainings.component.ts
+++ src/app/training/training/past-trainings/past-trainings.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Excercise } from '../excercise.model';
 import { TrainingService } from '../training.service';
@@ -9,15 +10,21 @@ import { TrainingService } from '../training.service';
   styleUrls: ['./past-trainings.component.css']
 })
 
-export class PastTrainingsComponent implements OnInit {
+export class PastTrainingsComponent implements OnInit, AfterViewInit {
 
-  displayedColumns: string[] = ['date', 'name', 'state', 'duration', 'calories'];
+  displayedColumns: string[] = ['position','date', 'name', 'state', 'duration', 'calories'];
   pastTrainingsData = new MatTableDataSource<Excercise>();
+
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(private theTrainingService: TrainingService) { }
 
   ngOnInit(): void {
     this.pastTrainingsData = this.theTrainingService.getPastTrainingsData();
+  }
+
+  ngAfterViewInit() {
+    this.pastTrainingsData.sort = this.sort;
   }
 
 }

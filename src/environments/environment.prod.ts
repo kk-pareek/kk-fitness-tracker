3b42b4df76c4cc40977fd3cd5abe5bcb36a202db
Krishan Kumar Pareek <k.k.pareek18@gmail.com>
2022-06-26 17:14:35 +0530

    Changes for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAlfo5cnaCfC_Sb_AiiR-SuU8E-mVrv8bs",
+    authDomain: "kk-fitness-tracker.firebaseapp.com",
+    projectId: "kk-fitness-tracker",
+    storageBucket: "kk-fitness-tracker.appspot.com",
+    messagingSenderId: "329637449679",
+    appId: "1:329637449679:web:512cd0cf9b9bbc9683ccba",
+    measurementId: "G-JJJH6X5DTB"
+  }
 };

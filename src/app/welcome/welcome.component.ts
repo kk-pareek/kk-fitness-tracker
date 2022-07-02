bb1a1f9ca4dcf8f0c38abad9394ccb2f0ddffa58
Krishan Kumar Pareek <k.k.pareek18@gmail.com>
2022-07-02 14:52:59 +0530

    Update landing page

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  currentQuote!: string;
+  quotesCollection = [
+    "Don't stop pal!!",
+    "Pain is temporary!!",
+    "Hustle each moment!!",
+    "Make it happen!!",
+    "You are the one!!",
+    "Live, don't just exist!!"
+  ]
+
+  constructor(private theRouter: Router) { }
 
   ngOnInit(): void {
+    this.currentQuote = this.quotesCollection[Math.floor(this.getRandomIndex(1, 5))-1];
   }
 
+  getRandomIndex(start: number, end: number) {
+    return Math.random() * (end - start) + start;
+  }
+
+  routeToSignUp() {
+    this.theRouter.navigate(['/signup']);
+  }
 }
